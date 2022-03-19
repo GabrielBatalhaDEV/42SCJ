7309e52926b0febcf290aa195efadd9f083f65c2
Gabriel Batalha <71796398+GabrielBatalhaDEV@users.noreply.github.com>
2022-03-19 06:56:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-const display = document.getElementById('display');
-const result = document.getElementById('igual');
-const teclasNumericas = document.querySelectorAll('[id*=tecla]');
-const teclasOperadoras = document.querySelectorAll('[id*=operador]');
+const display = document.getElementById("display");
+const result = document.getElementById("igual");
+const teclasNumericas = document.querySelectorAll("[id*=tecla]");
+const teclasOperadoras = document.querySelectorAll("[id*=operador]");
 
 let numeroNovo = true;
 let operador;
 let numeroAnterior;
 
-const atualizarDisplay = (texto)  => {
-    if(numeroNovo) {
-        display.textContent = texto;
-        numeroNovo = false;
-    }
-    else display.textContent += texto;
-}
+const atualizarDisplay = (texto) => {
+  if (numeroNovo) {
+    display.textContent = texto;
+    numeroNovo = false;
+  } else display.textContent += texto;
+};
 
 const inserirNumero = (event) => atualizarDisplay(event.target.textContent);
 
-teclasNumericas.forEach((tecla) => tecla.addEventListener('click', inserirNumero));
+teclasNumericas.forEach((tecla) =>
+  tecla.addEventListener("click", inserirNumero)
+);
 
 const selecionaOperador = (event) => {
-    numeroNovo = true;
-    operador = event.target.textContent;
-    numeroAnterior = display.textContent;
-}
+  numeroNovo = true;
+  operador = event.target.textContent;
+  numeroAnterior = display.textContent;
+};
 
-teclasOperadoras.forEach((tecla) => tecla.addEventListener('click', selecionaOperador));
+teclasOperadoras.forEach((tecla) =>
+  tecla.addEventListener("click", selecionaOperador)
+);
 
 const calcular = () => {
-    const numeroAtual = display.textContent;
-    const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`); //usando template string
-    numeroNovo = true;
-    atualizarDisplay(resultado);
-}
+  const numeroAtual = display.textContent.replace(",", ".");
+  numeroAnterior = numeroAnterior.replace(",", ".");
+  const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`); //usando template string
+  numeroNovo = true;
 
-result.addEventListener('click', calcular);
+  atualizarDisplay(String(resultado).replace(".", ","));
+};
+
+result.addEventListener("click", calcular);
 
 const limparDisplay = () => (display.textContent = "");
 
-document.querySelector("#limparDisplay").addEventListener("click", limparDisplay);
+document
+  .querySelector("#limparDisplay")
+  .addEventListener("click", limparDisplay);
 
 const limparCalculo = () => {
   limparDisplay();
@@ -47,16 +54,27 @@ const limparCalculo = () => {
   numeroAnterior = undefined;
 };
 
-document.querySelector("#limparCalculo").addEventListener("click", limparCalculo);
+document
+  .querySelector("#limparCalculo")
+  .addEventListener("click", limparCalculo);
 
-const removerUltimoNumero = () => 
-    (display.textContent = display.textContent.slice(0,-1));
+const removerUltimoNumero = () =>
+  (display.textContent = display.textContent.slice(0, -1));
 
-document.querySelector("#backspace").addEventListener("click", removerUltimoNumero);
+document
+  .querySelector("#backspace")
+  .addEventListener("click", removerUltimoNumero);
 
 const inverterSinal = () => {
-    numeroNovo = true;
-    atualizarDisplay(display.textContent * -1);
-}
+  numeroNovo = true;
+  atualizarDisplay(display.textContent * -1);
+};
 
 document.querySelector("#inverter").addEventListener("click", inverterSinal);
+
+const decimal = () => {
+  if (!display.textContent || numeroNovo) atualizarDisplay("0,");
+  else atualizarDisplay(",");
+};
+
+document.querySelector("#decimal").addEventListener("click", decimal);
